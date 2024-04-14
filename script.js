5a4888d66735bc31184493c364b981de844ba987
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-04-15 02:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const browserInfo =  document.getElementById("browser-info");
+browserInfo.innerText = "You are using " + navigator.appName + " version " + navigator.appVersion;
+
+// console.log(navigator.userAgent);
+// console.log(navigator.appName);
+// console.log(navigator.userAgent);
